8b2088a4448d5d8d1159e514bc4339aa3e00bb75
ksundev <kshk08@hanyang.ac.kr>
2025-06-25 15:28:50 +0900

    feat: "vue3-cli" 기본프로젝트 예제 추가

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <AppHeader :appTitle="message" @change="changeMessage"/>
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue'
+
+  export default {
+    components: {
+      "AppHeader": AppHeader
+    },
+    data() {
+      return {
+        message: "앱 헤더 컴포넌트"
+      }
+    },
+    methods: {
+      changeMessage() {
+        this.message = "앱 헤더 컴포넌트 변경"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
