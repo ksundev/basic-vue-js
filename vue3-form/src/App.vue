cead124dbc51702c8e56eda525913a5e89954ecb
ksundev <kshk08@hanyang.ac.kr>
2025-06-29 14:52:35 +0900

    feat: 새로운 프로젝트 vue3-form 생성 및 초기화면 구성

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userId">아이디:</label>
+      <input id="userId" type="text" v-model="userId">
+    </div>
+    <div>
+      <label for="password">비밀번호:</label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        userId: '',
+        password: ''
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
